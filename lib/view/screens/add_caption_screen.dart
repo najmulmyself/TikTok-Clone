bc5582f770a380dd5aa2a98d995d3a88f5c044a2
najmulmyself <najmul.myself@gmail.com>
2023-05-11 03:12:02 +0600

    added caption

--- lib/view/screens/add_caption_screen.dart
+++ lib/view/screens/add_caption_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constant.dart';
+import 'package:tiktok_clone/view/widgets/myTextfield.dart';
 import 'package:video_player/video_player.dart';
 
 class AddCaptionScreen extends StatefulWidget {
@@ -14,9 +16,9 @@ class AddCaptionScreen extends StatefulWidget {
 }
 
 class _AddCaptionScreenState extends State<AddCaptionScreen> {
-  late VideoPlayerController videoPlayerController =
-      VideoPlayerController.file(widget.videoFile);
-
+  late VideoPlayerController videoPlayerController;
+  TextEditingController songController = TextEditingController();
+  TextEditingController captionController = TextEditingController();
   @override
   void initState() {
     // TODO: implement initState
@@ -31,12 +33,48 @@ class _AddCaptionScreenState extends State<AddCaptionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: VideoPlayer(videoPlayerController),
-        )
-      ]),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.4,
+              child: VideoPlayer(videoPlayerController),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 4,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextInputField(
+                    controller: songController,
+                    myIcon: Icons.music_note,
+                    myLabelText: "Soong name",
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextInputField(
+                    controller: captionController,
+                    myIcon: Icons.closed_caption,
+                    myLabelText: "Caption",
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: buttonColor),
+                    onPressed: () {},
+                    child: const Text(
+                      "Upload",
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
